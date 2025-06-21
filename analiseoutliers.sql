4cf95f5eb13368a55e692dc8b16b559058fc3d48
Carolina Barcellos <carolinapbarcellos@gmail.com>
2025-06-21 09:53:13 -0300

    Add files via upload

---
+++ analiseoutliers.sql
@@ -0,0 +1,49 @@
+--ANÁLISE OUTLIERS--
+
+---------quantidade de complexos por uf------------------------------------------
+COPY (
+	SELECT uf_complexo, COUNT(*)
+	FROM complexo
+	GROUP BY uf_complexo
+	ORDER BY COUNT(*) DESC
+) TO '/Users/carolinabarcellos/saidas/complexos_por_uf.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+------quantidade de assentos por sala------------------------------------------------------
+COPY (
+SELECT nome_sala, assentos_sala
+from sala
+order by assentos_sala desc
+) TO '/Users/carolinabarcellos/saidas/sala_assentos.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+-----publico total por filme----------------------------------------------------------------
+COPY (
+select titulo_original, sum(publico) as publico_total
+from filme f natural join secao s
+group by titulo_original
+order by sum(publico) desc
+) TO '/Users/carolinabarcellos/saidas/filme_publico.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+-----quantidade de filmes por distribuidora-------------------------------------------------
+COPY (
+select d.razao_social_distribuidora, count(s.cpb_roe) as quantidade_filmes
+from protocolo p, secao s, distribuidora d 
+where p.cnpj_distribuidora =d.cnpj_distribuidora
+and p.nr_protocolo_envio =s.nr_protocolo_envio
+group by d.razao_social_distribuidora 
+order by count(s.cpb_roe) desc
+) TO '/Users/carolinabarcellos/saidas/filme_distribuidoras.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+---- publico total por distribuidora----------------------------------------------------------
+COPY (
+select razao_social_distribuidora, sum(publico) as publico_total
+from protocolo p, secao s, distribuidora d 
+where p.cnpj_distribuidora =d.cnpj_distribuidora
+and p.nr_protocolo_envio =s.nr_protocolo_envio
+group by d.razao_social_distribuidora 
+order by sum(publico) desc
+) TO '/Users/carolinabarcellos/saidas/publico_distribuidoras.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
---
+++ consultasextras.sql
@@ -0,0 +1,20 @@
+--CONSULTAS EXTRAS--
+
+-----quantidade de complexos itinerantes e não itenerantes-------------------
+COPY (
+	SELECT complexo_itinerante, COUNT(*)
+	FROM complexo
+	GROUP BY complexo_itinerante
+	ORDER BY COUNT(*) DESC
+) TO '/Users/carolinabarcellos/saidas/complexos_itinerantes_comparacao.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+
+-----quantidade de complexos em funcionamento, fechados e fechados temporariamente ----------------------
+COPY (
+SELECT situacao_complexo, count(*) as quantidade
+from complexo
+group by situacao_complexo
+order by count(*) desc
+) TO '/Users/carolinabarcellos/saidas/situacao_complexo.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
